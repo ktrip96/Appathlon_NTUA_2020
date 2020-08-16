6658ae637e18d0d859a674212d3ad8f00daccd09
ktrip96 <ktrip96@gmail.com>
2020-08-16 14:39:06 +0300

    Added Testing Google Maps

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-export default function Map() {
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 37.9908997,
+      lng: 23.70332
+    },
+    zoom: 12
+  };
+
+  render() {
     return (
-        <div>
-            Map Component Under Construction
-        </div>
-    )
+      <div style={{ height: '700px', width: '800px' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyD1ngvuac07TSdSVKpvctQS-wwHkwa4FO8' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={37.9908997}
+            lng={23.70332}
+            text=""
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
+
+export default SimpleMap;
