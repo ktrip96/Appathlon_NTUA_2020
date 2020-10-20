25b04b3a30dfab8e77652a8de24a56cf4e59477e
ktrip96 <ktrip96@gmail.com>
2020-10-20 09:24:43 +0300

    Java Web Service Added

---
+++ Java Web Service/src/main/java/com/ntua/demo/GreetingController.java
@@ -0,0 +1,20 @@
+package com.ntua.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    Greeting greeting(@RequestParam(defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
